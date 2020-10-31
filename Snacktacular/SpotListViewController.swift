e8256daf43447df056c7512b4157da07bad33904
Brenden Picioane <brenden.picioane@gmail.com>
2020-10-31 18:50:14 -0400

    finished 8.4, more UI details

--- Snacktacular/SpotListViewController.swift
+++ Snacktacular/SpotListViewController.swift
@@ -8,35 +8,33 @@
 
 import UIKit
 
-class SpotsListViewController: UIViewController {
+class SpotListViewController: UIViewController {
 
     var spots = ["El Pelon", "Pino's Pizza", "Kung Fu Tea"]
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var sortSegmentedControl: UISegmentedControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         tableView.delegate = self
         tableView.dataSource = self
-
-        // Do any additional setup after loading the view.
+        configureSegmentedControl()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func configureSegmentedControl() {
+        let orangeFontColor = [NSAttributedString.Key.foregroundColor : UIColor(named: "PrimaryColor")!]
+        let whiteFontColor = [NSAttributedString.Key.foregroundColor : UIColor.white]
+        sortSegmentedControl.setTitleTextAttributes(orangeFontColor, for: .selected)
+        sortSegmentedControl.setTitleTextAttributes(whiteFontColor, for: .normal)
+        sortSegmentedControl.layer.borderColor = UIColor.white.cgColor
+        sortSegmentedControl.layer.borderWidth = 1.0
+        
     }
-    */
 
 }
 
-extension SpotsListViewController: UITableViewDataSource, UITableViewDelegate {
+extension SpotListViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return spots.count
     }
