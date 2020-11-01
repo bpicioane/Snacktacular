42d270bc1ac6ab65c63729ccd68cd2b1fb5e6d71
Brenden Picioane <brenden.picioane@gmail.com>
2020-11-01 11:53:41 -0500

    finished 8.6, loading data from Firestore

--- Snacktacular/SpotListViewController.swift
+++ Snacktacular/SpotListViewController.swift
@@ -10,16 +10,24 @@ import UIKit
 
 class SpotListViewController: UIViewController {
 
-    var spots = ["El Pelon", "Pino's Pizza", "Kung Fu Tea"]
+    var spots: Spots!
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var sortSegmentedControl: UISegmentedControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        spots = Spots()
         tableView.delegate = self
         tableView.dataSource = self
         configureSegmentedControl()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        spots.loadData {
+            self.tableView.reloadData()
+        }
     }
     
     func configureSegmentedControl() {
@@ -29,19 +37,26 @@ class SpotListViewController: UIViewController {
         sortSegmentedControl.setTitleTextAttributes(whiteFontColor, for: .normal)
         sortSegmentedControl.layer.borderColor = UIColor.white.cgColor
         sortSegmentedControl.layer.borderWidth = 1.0
-        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowDetail" {
+            let destination = segue.destination as! SpotDetailViewController
+            let selectedIndexPath = tableView.indexPathForSelectedRow!
+            destination.spot = spots.spotArray[selectedIndexPath.row]
+        }
     }
 
 }
 
 extension SpotListViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return spots.count
+        return spots.spotArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! SpotTableViewCell
-        cell.nameLabel?.text = spots[indexPath.row]
+        cell.nameLabel?.text = spots.spotArray[indexPath.row].name
         return cell
     }
     
