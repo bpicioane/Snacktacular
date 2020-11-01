13492c899e56294043fd3bfe1651bde5c395e4ce
Brenden Picioane <brenden.picioane@gmail.com>
2020-10-31 22:56:53 -0400

    switched firebase accounts

---
+++ Snacktacular/Spot.swift
@@ -0,0 +1,28 @@
+//
+//  Spot.swift
+//  Snacktacular
+//
+//  Created by Brenden Picioane on 10/31/20.
+//  Copyright © 2020 Brenden Picioane. All rights reserved.
+//
+
+import Foundation
+
+class Spot {
+    var name: String
+    var address: String
+    var averageRating: Double
+    var numberOfReviews: Int
+    var postingUserID: String
+    var documentID: String
+    
+    init(name: String, address: String, averageRating: Double, numberOfReviews: Int, postingUserID: String, documentID: String) {
+        self.name = name
+        self.address = address
+        self.averageRating = averageRating
+        self.numberOfReviews = numberOfReviews
+        self.postingUserID = postingUserID
+        self.documentID = documentID
+    }
+    
+}
