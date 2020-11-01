53fcafefa691d37576ee1479328b48c8eb83b938
Brenden Picioane <brenden.picioane@gmail.com>
2020-11-01 12:41:37 -0500

    finished 8.7, Google Place Autocomplete

---
+++ Snacktacular/APIKeys.swift
@@ -0,0 +1,13 @@
+//
+//  APIKeys.swift
+//  Snacktacular
+//
+//  Created by Brenden Picioane on 11/1/20.
+//  Copyright © 2020 Brenden Picioane. All rights reserved.
+//
+
+import Foundation
+
+struct APIKeys {
+    static let googlePlacesKey = "AIzaSyCGXQgDTfxboae7Z8hhUa2oGHSQY5DGpqs"
+}
